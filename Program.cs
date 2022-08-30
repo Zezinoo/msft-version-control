890f9e15f4bf66cef565f95495b64c27d7e4070d
josevitor <josevitoralves561@gmail.com>
2022-08-30 19:59:22 -0300

    Alteraçao feita por Jose

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 string valor = null;
 string s2 = valor ?? "nothing";
+//Alteraçao feita por Jose
 valor = valor?.Trim().ToUpper();
 Console.WriteLine(s2);
