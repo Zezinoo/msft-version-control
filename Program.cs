70b06ca42c16a2b58097278bc3d848c58426ba4b
Gabriel Viterbo <setokaibaboi@gmail.com>
2022-08-30 20:00:11 -0300

    alteração feita por Gabriel

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 string valor = null;
 string s2 = valor ?? "nothing";
+//Alteração feita por Gabriel
 valor = valor?.Trim().ToUpper();
 Console.WriteLine(s2);
